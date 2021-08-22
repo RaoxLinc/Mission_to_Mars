6ef3ccfdaffe6b9bcc6a30b06db9ca08a864e374
xLinc x <LCheng2413@yahoo.com>
2021-08-22 17:12:31 -0400

    mesage

---
+++ app.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    # Run scrapped functions
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_mars_news()
+    mars_data = scrape_mars.scrape_mars_image()
+    mars_data = scrape_mars.scrape_mars_facts()
+    mars_data = scrape_mars.scrape_mars_weather()
+    mars_data = scrape_mars.scrape_mars_hemispheres()
+    mars_info.update({}, mars_data, upsert=True)
+    return redirect("/")
+      
+if __name__ == "__main__":
+   app.run(debug=True)
+
